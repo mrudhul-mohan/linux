cb3b9d8951ad34ac9198e2d228fd94e2974f1ac1
mrudhul-mohan <67993484+mrudhul-mohan@users.noreply.github.com>
2021-08-26 12:44:00 +0530

    Create ssh_auto_login_template.sh

---
+++ ssh_auto_login_template.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SSHPASS="value"
+
+## Server login details
+
+SERVER_IP="ip"
+USERNAME="user_name"
+SSHPORT="port_number"
+PASSWORD="passwd"
+KEY=""
+EXTRA_OPT="-o LogLevel=ERROR -o StrictHostKeyChecking=no -o ControlMaster=auto -o ControlPath="~/.ssh/%r@%h-%p" -o ControlPersist=300"
+
+if [ $SSHPASS = yes ]
+
+then
+        sshpass -p ${PASSWORD} ssh -D8080 ${EXTRA_OPT} ${USERNAME}@${SERVER_IP} -p${SSHPORT}
+
+else
+
+
+        ssh ${EXTRA_OPT} -i ${KEY} ${USERNAME}@${SERVER_IP} -p${SSHPORT}
+
+
+fi
